d6dbbca3ba63f39397ef1fc1e7e94c9ce6563180
Aubrey Tull <aubrey@stem.ai>
2024-07-20 10:34:06 -0700

    Test assertion

--- src/doc/docs.test.ts
+++ src/doc/docs.test.ts
@@ -1,14 +1,13 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, it } from "@jest/globals";
 import { splitDoc } from "./doc";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 describe("splitDoc", () => {
-  test("splitDoc", async () => {
+  it("should produce non-empty list of documents", async () => {
     const docs = await splitDoc(process.env.ASSET_FILE_PATH!);
-    docs.forEach((text) => {
-      console.log(text.pageContent.slice(0, 50));
-    });
+    expect(docs.length).not.toBe(0);
+    expect(docs).not.toEqual(undefined);
   });
 });
